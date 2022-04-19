44e05457a786dcf38e89e83bd86faeee1750d9de
Michael <michael@sensemaking.solutions>
2022-04-19 09:13:13 +0700

    fixed key and added create game account

--- src/domain/Command/Create_Account.ts
+++ src/domain/Command/Create_Account.ts
@@ -14,7 +14,30 @@ export class Create_Account implements IDomainEvent {
 
         const domainEventClass = Reflect.getPrototypeOf(this);
         this.EventType = domainEventClass.constructor.name;
-        this.key = [`orders.command.create_order`];
+        this.key = [`orders.command.create_account`];
+        this.dateTimeOccurred = new Date();
+        this.UserId = account.UserId;
+        this.Id = account.id
+    }
+
+    getAggregateId (): UniqueEntityID {
+        return this.Id;
+    }
+}
+
+
+export class Create_Game_Account implements IDomainEvent {
+    public key:string[];
+    public EventType: string;
+    public dateTimeOccurred: Date;
+    public UserId: string;
+    public Id: UniqueEntityID;
+
+    constructor (account: Account) {
+
+        const domainEventClass = Reflect.getPrototypeOf(this);
+        this.EventType = domainEventClass.constructor.name;
+        this.key = [`orders.command.create_game_account`];
         this.dateTimeOccurred = new Date();
         this.UserId = account.UserId;
         this.Id = account.id
