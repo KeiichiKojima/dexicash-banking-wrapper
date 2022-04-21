b451382e621082039c61084becd8bf1da0a6d24f
Keiichi <keiichikojima365@gmail.com>
2022-04-21 04:24:36 +0000

    remove auth in mongodb

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,7 +1,7 @@
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const { DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 const dbConfig = {
-    url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+    url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
 }
 
 console.log(dbConfig.url);
